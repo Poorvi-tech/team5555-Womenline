4064ff0208a60b317a8c9e333b65bcc1c010af04
vaishali912 <kunwarvaishali@gmail.com>
2025-07-14 22:42:05 +0530

    Models  Design

--- womenline-backend/app.js
+++ womenline-backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+const connectDB = require('./config/db.js');
+connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,7 +13,10 @@ app.get('/', (req, res) => {
   res.send('WomenLine backend is running');
 });
 
+
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
