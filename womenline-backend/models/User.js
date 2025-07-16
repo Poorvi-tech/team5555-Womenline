cb936c1974fe6f2c495fa032723dd944cd960c95
vaishali912 <kunwarvaishali@gmail.com>
2025-07-16 23:19:28 +0530

    Models degisn

---
+++ womenline-backend/models/User.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const Schema = mongoose.Schema;
+const User = new Schema({
+   name:{
+    type:String,
+    required:true
+   },
+    password:{
+    type:String,
+    required:true
+   },
+     role:{
+    type:String,
+    required:true
+   },
+   greenCredits:{
+    type:Number,
+    required:true
+   }
+}, { timestamps: true })
+
+module.exports = mongoose.model("User",User)
