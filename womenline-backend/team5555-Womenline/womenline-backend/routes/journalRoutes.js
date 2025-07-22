28e664c6701a887ff4aedaee8d2fc3e635b428f9
Anshika26-git <anshikasharma20018@gmail.com>
2025-07-23 01:02:37 +0530

    Removed nested womenline-backend structure and kept final backend folder

--- womenline-backend/team5555-Womenline/womenline-backend/routes/journalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const journalController = require('../controllers/journalController');
-const protect = require('../middleware/authMiddleware'); // correct path and import
-
-router.get('/', protect, journalController.getJournals);
-router.post('/', protect, journalController.createJournal);
-
-module.exports = router;
