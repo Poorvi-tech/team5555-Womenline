8b691c78ccd481740a41636aa8b0fe24d6874c82
Anshika26-git <anshikasharma20018@gmail.com>
2025-07-27 00:58:08 +0530

    Remove womenline-mongoose from repository but keep locally

--- womenline-backend/womenline-mongoose/team5555-Womenline/womenline-backend/routes/journalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const journalController = require('../controllers/journalController');
-const {protect,rolecheck} = require('../middleware/authMiddleware'); // correct path and import
-
-router.get('/', protect,rolecheck(['mother', 'caregiver', 'admin','user']), journalController.getJournals);
-router.post('/', protect,rolecheck(['mother', 'caregiver', 'admin','user']),  journalController.createJournal);
-
-module.exports = router;
